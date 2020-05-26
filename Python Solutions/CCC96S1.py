7af6a4c2aaf0dbc19cd72a7d816d05dd9360b3d9
HDCat2 <65579175+HDCat2@users.noreply.github.com>
2020-05-26 16:28:09 -0400

    Create CCC96S1.py

---
+++ Python Solutions/CCC96S1.py
@@ -0,0 +1,10 @@
+n=input()
+for i in range(n):
+    a=input()
+    s=sum([n for n in range(1,a) if a%n==0])
+    if s == a:
+        print "%i is a perfect number." % (a)
+    elif s > a:
+        print '%i is an abundant number.' % (a)
+    else:
+        print '%i is a deficient number.' % (a)
